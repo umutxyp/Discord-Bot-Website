ab9f371132837abd0dfbd634b3c4bc672de19316
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2023-11-22 17:41:57 +0300

    Update ErrorPage.jsx

--- components/ErrorPage.jsx
+++ components/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = ({ code, message }) => {
     
     return <>
         <Head>
-            <title>{code} | Astra Bot</title>
+            <title>{code} | MusicMaker</title>
         </Head>
 
         <div className="max-w-7xl text-center py-20 mx-auto">
