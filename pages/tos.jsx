68de539e7550f693f49e5e7fe248e20d8ebe42ae
mrdomates7878 <111639740+mrdomates7878@users.noreply.github.com>
2024-05-06 14:05:21 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,18 +4,16 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Her discord sunucusu Frex botunu ücretsiz olarak kullanabilir, ücret karşılığında satışı veya pazarlaması kesinlikle yasaktır.</p>
 <div className="py-3"></div>
-<p>⊳ You cannot use bot commands to harm the bot.</p>
+<p>⊳ Bot komutlarını bota zarar vermek için kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
+<p>⊳ Açık kaynak kodlarını diğer sunuculara zarar verecek şekilde kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Açık kaynak kodlarını başka platformlar üzerinden satamaz veya pazarlayamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
+<p>⊳ Botu kullanırken diğer kullanıcılara zarar verecek şekilde troll amaçlı kullanamazsınız.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
-</div>
     </div>
   );
 }
