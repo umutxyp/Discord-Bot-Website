d4405b1b10cbf7a38bacc9c4c7fada9d9896eaac
rothanak <rothanak.thoeurn@cyder.page>
2024-07-30 19:44:22 +0700

    update size

--- components/Static/Header.jsx
+++ components/Static/Header.jsx
@@ -13,7 +13,7 @@ const MobileNavbar = ({ open, setOpen, NavItems }) => {
 
                 <div className="flex justify-between border-b border-white/20 items-center px-5 py-4">
                     <div className="flex items-center">
-                        <img width="32" className="rounded-full icon-glow" src="/img/logo.png" alt={'logo'}/>
+                        <img width="1080" className="rounded-full icon-glow" src="/img/logo.png" alt={'logo'}/>
                         <p className=" font-extrabold ml-1 text-2xl">DD Master</p>
                     </div>
                     <button onClick={() => setOpen(!open)}><i className="cursor-pointer fa fa-times text-xl mr-2" /></button>
@@ -80,7 +80,7 @@ const Header = ({ $, NavItems }) => {
                                  className={`
                                     rounded-full 
                                 `}
-                                 width="1080" height="1080"
+                                 width={"48"} height={"48"}
                              alt={'second-logo'}/>
                             <p className="invisible md:visible text-xl text-white font-semibold">
                               <a href="/"><span className="text-amber-400">DD</span> Master</a>
