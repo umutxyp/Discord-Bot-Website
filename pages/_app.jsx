9d86172f865a7559cfeb3fa34483675d8ec0cb7d
rothanak <rothanak.thoeurn@cyder.page>
2024-07-30 20:00:07 +0700

    update size

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,14 +37,14 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/8s2xubrDSq",
     },
     {
       link: true,
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/oauth2/authorize?client_id=1255576744577208401",
     },
     //    {
     //   link: true,
