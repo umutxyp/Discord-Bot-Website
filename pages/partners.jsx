735165ca831fc02cd380536656d36d485628a2db
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-12-04 23:35:36 +0300

    Update partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,29 +18,12 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://codeshare.me">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://cdn.discordapp.com/icons/676124992919830548/72826a4ed7d1a010df263623be5dd4ba.png" />
+                            <img className="rounded-full h-32 w-32" src="https://images-ext-1.discordapp.net/external/YYXCkfsbbSARvGEzBx8W_3gt7ITp4f8YB_2y14SpoiA/%3Fsize%3D4096/https/cdn.discordapp.com/icons/676124992919830548/256b9dd9a6109b1c85ca64677f84f80a.png?format=webp&quality=lossless&width=160&height=160" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
                                       Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.
-                                    </p>
-                                </div>     
-                            </div>
-                        </div>
-                    </a>
-                    </div>
-
-
-                            <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://codeshare.me/partners/64db583a4292ca1066ad0a1b">
-                        <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1172657853056684102/1172671829673005146/maskot_profil_alev.png" />
-                            <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Nodesty VDS</h1>        
-                                <div className="flex items-center mt-1">
-                                    <p className="font-normal font-sm">
-                                   DISCOUNT CODE: CODESHARE Experience uninterrupted internet speed up to 10Gb and cheap intel i9, amd ryzen series vds with Nodesty. Shop with Code Share assurance.
                                     </p>
                                 </div>     
                             </div>
