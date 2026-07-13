7e23a23d589c671ef94c182573a250a34fba554b
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2026-07-13 10:52:07 +0700

    Delete pages directory

--- pages/500.jsx
+++
@@ -1,6 +0,0 @@
-import ErrorPage from '../components/ErrorPage';
-const UnkownPage = () => {
-    return <ErrorPage code={500} message={"Oh. It seems there is a 500 Internal Server Error. Please try again later!"} />
-}
-
-export default UnkownPage;
